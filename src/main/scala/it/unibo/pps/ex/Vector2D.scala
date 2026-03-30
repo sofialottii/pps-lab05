115b40c17a6a8c8ebbfd422d67a57152a7294b73
Sofia <irenesofia.lotti@studio.unibo.it>
2026-03-30 10:11:42 +0200

    imeplemented ex1: Vector2D

--- src/main/scala/it/unibo/pps/ex/Vector2D.scala
+++ src/main/scala/it/unibo/pps/ex/Vector2D.scala
@@ -24,8 +24,21 @@ trait Vector2D:
   def magnitude: Double
 
 object Vector2D:
+
+  private class Vector2DImpl(val x: Double, val y: Double) extends Vector2D {
+
+    override def +(other: Vector2D): Vector2D = Vector2D(x+other.x, y+other.y)
+
+    override def -(other: Vector2D): Vector2D = Vector2D(x-other.x, y-other.y)
+
+    override def *(scalar: Double): Vector2D = Vector2D(x*scalar, y*scalar)
+
+    override def dot(other: Vector2D): Double = x*other.x + y*other.y
+
+    override def magnitude: Double = sqrt(x*x + y*y)
+  }
   // Factory method to create Vector2D instances
-  def apply(x: Double, y: Double): Vector2D = ???
+  def apply(x: Double, y: Double): Vector2D = Vector2DImpl(x, y)
 
   // Common vectors (optional but nice)
   val zero: Vector2D = apply(0.0, 0.0)
